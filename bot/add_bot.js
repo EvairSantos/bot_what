c09a131cd65c3d746e11d415821cacd4311ca5b0
Evair Santos de Andrade <108219985+EvairSantos@users.noreply.github.com>
2024-06-20 17:50:10 -0300

    Update add_bot.js

--- bot/add_bot.js
+++ bot/add_bot.js
@@ -15,7 +15,7 @@ async function adicionarBotWhatsApp() {
         await page.goto('https://web.whatsapp.com', { waitUntil: 'networkidle0' });
 
         while (true) {
-            await page.waitForSelector('div._akau', { timeout: 120000 });
+            await page.waitForSelector('div._akau', { timeout: 60000 });
             const qrContent = await page.evaluate(() => {
                 const qrElement = document.querySelector('div._akau');
                 return qrElement ? qrElement.getAttribute('data-ref') : null;
@@ -25,7 +25,7 @@ async function adicionarBotWhatsApp() {
                 console.log('QR code capturado, exibindo no terminal...');
                 qrcode.generate(qrContent, { small: true });
 
-                await page.waitForSelector('div._al_c', { timeout: 120000 });
+                await page.waitForSelector('div._alyr', { timeout: 60000 });
                 console.log('Código QR escaneado com sucesso! WhatsApp Web conectado.');
 
                 const isQRCodeRead = await page.evaluate(() => {
@@ -35,8 +35,10 @@ async function adicionarBotWhatsApp() {
 
                 if (isQRCodeRead) {
                     console.log('Bot adicionado como novo dispositivo!');
-                    // Aqui você pode adicionar qualquer ação adicional após a conexão bem-sucedida
-                    break; // Encerra o loop porque o bot está ativo
+                    break;
+                } else {
+                    console.log('Aguardando o QR code ser lido...');
+                    await page.waitForTimeout(5000); // Aguardar 5 segundos e verificar novamente
                 }
             } else {
                 throw new Error('Não foi possível capturar o QR code.');
