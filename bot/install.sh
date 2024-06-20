8008b8735c0e3e079e9201e587e66f3b7f3c2ce1
Evair Santos de Andrade <108219985+EvairSantos@users.noreply.github.com>
2024-06-20 10:30:00 -0300

    Update install.sh

--- bot/install.sh
+++ bot/install.sh
@@ -25,10 +25,11 @@ base_dir=$(pwd)
 print_status "Baixando e extraindo o repositório..."
 curl -fsSL https://github.com/EvairSantos/bot_what/archive/master.zip -o bot_what.zip
 unzip -q bot_what.zip
-project_dir="$base_dir/bot_what-master"
+project_dir="$base_dir/bot_what-main/bot_what-master"
 
-# Navegar até o diretório do projeto
-cd "$project_dir" || exit
+# Remover o arquivo ZIP após a extração
+print_status "Removendo arquivo ZIP..."
+rm bot_what.zip
 
 # Verificar se o diretório bot já existe, se não, criar
 if [ ! -d "bot" ]; then
@@ -37,11 +38,14 @@ fi
 
 # Copiar todos os arquivos da pasta bot do repositório para o diretório bot
 print_status "Copiando arquivos para o diretório bot..."
-yes | cp -rf bot_what-master/bot/. bot/
+yes | cp -rf "$project_dir/bot/." bot/
+
+# Navegar até o diretório do projeto
+cd "$project_dir" || exit
 
 # Instalar as dependências do projeto
 print_status "Instalando as dependências..."
-npm install
+npm install --quiet
 
 # Configurar o arquivo .env (exemplo: definindo a porta)
 print_status "Configurando o arquivo .env..."
@@ -49,7 +53,7 @@ echo "PORT=3000" > .env
 
 # Gerar o QR Code na tela da VPS
 print_status "Gerando o código QR para escanear..."
-npm install qrcode-terminal
+npm install --quiet qrcode-terminal
 
 # Script para gerar o QR Code na linha de comando
 node <<EOF
