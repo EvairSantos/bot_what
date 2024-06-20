1844e3204554010b2cd94f2c7ce8728a46e1b43f
Evair Santos de Andrade <108219985+EvairSantos@users.noreply.github.com>
2024-06-20 20:02:16 -0300

    Update add_bot.js

--- bot/add_bot.js
+++ bot/add_bot.js
@@ -16,36 +16,28 @@ async function adicionarBotWhatsApp() {
 
         let qrCodeFound = false;
 
-        while (!qrCodeFound) {
-            try {
-                console.log('Aguardando o QR code...');
-                // Espera até que o elemento com atributo data-ref seja visível
-                await page.waitForSelector('div[data-ref]', { timeout: 60000 });
+        // Aguarda até que o elemento com atributo data-ref seja visível
+        await page.waitForSelector('div[data-ref]', { timeout: 0 });
 
-                // Captura o atributo data-ref do elemento
-                const qrContent = await page.evaluate(() => {
-                    const qrElement = document.querySelector('div[data-ref]');
-                    return qrElement ? qrElement.getAttribute('data-ref') : null;
-                });
+        console.log('Aguardando o QR code...');
 
-                if (qrContent) {
-                    console.log('QR code capturado, exibindo no terminal...');
-                    qrcode.generate(qrContent, { small: true });
+        // Captura o atributo data-ref do elemento
+        const qrContent = await page.evaluate(() => {
+            const qrElement = document.querySelector('div[data-ref]');
+            return qrElement ? qrElement.getAttribute('data-ref') : null;
+        });
 
-                    // Aguarda até que o elemento após o QR code seja visível
-                    await page.waitForSelector('div#wa-popovers-bucket', { timeout: 60000 });
+        if (qrContent) {
+            console.log('QR code capturado, exibindo no terminal...');
+            qrcode.generate(qrContent, { small: true });
 
-                    console.log('Dispositivo adicionado com sucesso!');
-                    qrCodeFound = true; // Marca que o QR code foi encontrado e o dispositivo foi adicionado
-                } else {
-                    console.log('Não foi possível capturar o QR code. Tentando novamente...');
-                    await page.waitForTimeout(5000); // Aguarda 5 segundos e tenta novamente
-                }
-            } catch (error) {
-                console.error('Erro ao capturar o QR code ou verificar o dispositivo:', error);
-                // Pode ser necessário atualizar a página aqui, caso o tempo de espera tenha sido excedido
-                await page.reload({ waitUntil: 'networkidle0' });
-            }
+            // Aguarda até que o elemento após o QR code seja visível
+            await page.waitForSelector('div#wa-popovers-bucket', { timeout: 0 });
+
+            console.log('Dispositivo adicionado com sucesso!');
+            qrCodeFound = true; // Marca que o QR code foi encontrado e o dispositivo foi adicionado
+        } else {
+            console.log('Não foi possível capturar o QR code.');
         }
     } catch (error) {
         console.error('Erro ao adicionar o bot como novo dispositivo:', error);
