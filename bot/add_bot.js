4fd2feb21ba7d2b8147e75b1fc19b482ff7ac11d
Evair Santos de Andrade <108219985+EvairSantos@users.noreply.github.com>
2024-06-20 16:53:31 -0300

    Update add_bot.js

--- bot/add_bot.js
+++ bot/add_bot.js
@@ -15,33 +15,28 @@ async function adicionarBotWhatsApp() {
         await page.goto('https://web.whatsapp.com', { waitUntil: 'networkidle0' });
 
         while (true) {
-            await page.waitForSelector('div._akau', { timeout: 60000 });
-            const qrContent = await page.evaluate(() => {
-                const qrElement = document.querySelector('div._akau');
-                return qrElement ? qrElement.getAttribute('data-ref') : null;
+            await page.waitForSelector('div._1CRbF', { timeout: 60000 }); // Espera pelo elemento após o login
+
+            const isLoggedIn = await page.evaluate(() => {
+                return document.querySelector('div._1CRbF') !== null; // Verifica se o elemento após o login está presente
             });
 
-            if (qrContent) {
-                console.log('QR code capturado, exibindo no terminal...');
-                qrcode.generate(qrContent, { small: true });
+            if (isLoggedIn) {
+                console.log('WhatsApp Web conectado após o login.');
 
-                await page.waitForSelector('._2Uw-r', { timeout: 120000 }); // Aumentado para 120 segundos
-                console.log('Código QR escaneado com sucesso! WhatsApp Web conectado.');
-
-                const isQRCodeRead = await page.evaluate(() => {
-                    const statusElement = document.querySelector('._2Uw-r');
-                    return statusElement ? statusElement.textContent.includes('Conectado') : false;
+                const isBotAdded = await page.evaluate(() => {
+                    return document.querySelector('div._2Uw-r') !== null; // Verifica se o bot foi adicionado
                 });
 
-                if (isQRCodeRead) {
+                if (isBotAdded) {
                     console.log('Bot adicionado como novo dispositivo!');
                     break;
                 } else {
-                    console.log('Aguardando o QR code ser lido novamente...');
-                    await page.waitForTimeout(5000); // Aguardar 5 segundos e verificar novamente
+                    console.log('Aguardando o bot ser adicionado...');
+                    await page.waitForTimeout(5000); // Aguarda 5 segundos e verifica novamente
                 }
             } else {
-                throw new Error('Não foi possível capturar o QR code.');
+                throw new Error('Erro ao detectar o login no WhatsApp Web.');
             }
         }
     } catch (error) {
